35e1118185b2321e3292b5c9e8bdd573479a57c0
JairDev <devalfredomoscoso@gmail.com>
2022-06-03 15:30:42 -0400

    Added move functionality

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+import gsap from "gsap"
+
 const hero = document.querySelector(".contain-mask-effect.copy")
 
-window.addEventListener("mousemove", () => {
-  console.log("e")
+window.addEventListener("mousemove", (e) => {
+  const x = Math.round(e.clientX / window.innerWidth * 100)
+  const y = Math.round(e.clientY / window.innerHeight * 100)
+  gsap.to(hero, {
+    '--x': `${x}%`,
+    '--y': `${y}%`,
+    duration: .5,
+    ease: 'sine.out'
+  })
   
 })
