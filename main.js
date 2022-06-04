6383b15b66509aac55f7d29d74a6ee1bd56b3c09
JairDev <devalfredomoscoso@gmail.com>
2022-06-04 16:05:43 -0400

    Remove navigation bar

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-import gsap from "gsap"
+import gsap from "gsap";
 
-const hero = document.querySelector(".contain-mask-effect.copy")
+const hero = document.querySelector(".contain-mask-effect.copy");
 
 window.addEventListener("mousemove", (e) => {
-  const x = Math.round(e.clientX / window.innerWidth * 100)
-  const y = Math.round(e.clientY / window.innerHeight * 100)
+  const x = Math.round((e.clientX / window.innerWidth) * 100);
+  const y = Math.round((e.clientY / window.innerHeight) * 100);
   gsap.to(hero, {
-    '--x': `${x}%`,
-    '--y': `${y}%`,
-    duration: .5,
-    ease: 'sine.out'
-  })
-  
-})
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.4,
+    ease: "sine.out",
+  });
+});
